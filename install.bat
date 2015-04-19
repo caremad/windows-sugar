db135c20af7e4ed2e4a54443438c71efbcc04af6
Nathan Winant <nw@exegetic.net>
2015-04-19 15:10:47 -0500

    Adds install.bat wrapper

---
+++ install.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set downloadDir="target"
+
+if exist "%downloadDir%\" (
+  choice /m "Download directory %downloadDir%'' already exists; do you want to delete it"
+  IF ERRORLEVEL 2 (
+    echo Aborting installation
+    exit /b
+  )
+  echo "Delete %downloadDir%"
+  @RD /S /Q "%downloadDir%"
+)
+
+echo "Proceeding..."s
+
+cscript install.vbs
+
+echo Done!
